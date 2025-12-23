d32cc305cf7110442b575350de73c789c77418c4
ubaid003 <anilakhan2619@gmail.com>
2025-12-22 23:49:42 -0800

    Time: 1 ms (94.74%) | Memory: 47.3 MB (27.79%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0); // dummy node
+        dummy.next = head;
+        ListNode current = dummy;
+
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next; // remove node
+            } else {
+                current = current.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
