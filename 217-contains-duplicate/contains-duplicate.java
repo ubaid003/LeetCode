91dbd87079ee8b703f39fd8083981d41c6e17503
ubaid003 <anilakhan2619@gmail.com>
2025-12-22 21:40:39 -0800

    Time: 14 ms (83.02%) | Memory: 93.2 MB (58.26%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true; // duplicate mil gaya
+            }
+            set.add(num);
+        }
+
+        return false; // koi duplicate nahi
+    }
+}
